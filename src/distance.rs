bb0fb48ac87a4e0ddaed1af071ccbda2c3177753
Sergueille Bismuth <sergueille@toaster.home>
2024-04-23 18:52:21 +0200

    distance: optimisations, some variable costs, but still way too slow

--- src/distance.rs
+++ src/distance.rs
@@ -1,9 +1,27 @@
 
 use crate::vec2d::Vec2D;
+extern crate unidecode;
+
+// TODO: better support for strange characters such as œ, for instance oe and œ should have a distance that is < 1, but the distance is currently 2
+
+/// Minimal value taken by insertion cost
+pub const MIN_INSERTION_COST: f32 = 1.0;
+/// Minimal value taken by deletion cost
+pub const MIN_DELETION_COST: f32 = 1.0;
+
+/// Cost of a substitution where a diacritic is ommited (e/é or c/ç)
+/// Low value because generally the user forget it, and it doesn't change completely the meaning of the word
+pub const DIACRITIC_SUBTITUTION_COST: f32 = 0.1;
+
+/// Cost of a substitution where letters are adjacent ok keyboard
+pub const ADJACENT_SUBTITUTION_COST: f32 = 0.5;
+
+/// Cost of a transposition where letters are adjacent ok keyboard
+pub const ADJACENT_TRANSPOSITION_COST: f32 = 0.5;
 
 /// Gets the edition distance between two strings
 /// 
-/// Uses https://bmcbioinformatics.biomedcentral.com/articles/10.1186/s12859-019-2819-0, with basic recursive dynamic programming
+/// Uses https://bmcbioinformatics.biomedcentral.com/articles/10.1186/s12859-019-2819-0, with basic recursive dynamic programming and variable costs
 pub fn get_distance(a: &str, b: &str) -> f32 {
     // Get characters
     let a_chars: Vec<char> = a.chars().collect();
@@ -45,7 +63,7 @@ pub fn get_distance_aux(memo: &mut Vec2D<Option<f32>>, a: &Vec<char>, b: &Vec<ch
 
                 let last_a = last_letter_id(a, b[j - 1], i - 1);
                 let last_b = last_letter_id(b, a[i - 1], j - 1);
-                
+
                 let transposition_cost = match (last_a, last_b) {
                     (Some(la), Some(lb)) => {
                         let base_cost = get_distance_aux(memo, a, b, la, lb);
@@ -64,7 +82,6 @@ pub fn get_distance_aux(memo: &mut Vec2D<Option<f32>>, a: &Vec<char>, b: &Vec<ch
                     },
                     _ => f32::INFINITY
                 };
-
 
                 res = f32::min(
                     f32::min(substitution_cost, insertiton_cost),
@@ -87,9 +104,16 @@ pub fn get_deletion_cost(c: char) -> f32 {
     return 1.0;
 }
 
+// Swap A with B
 pub fn get_substitution_cost(a: char, b: char) -> f32 {
     if a == b {
         return 0.0;
+    }
+    else if are_characters_equal_with_diacritics(a, b) {
+        return DIACRITIC_SUBTITUTION_COST;
+    }
+    else if are_characters_adjacent_on_keyboard(a, b) {
+        return ADJACENT_SUBTITUTION_COST;
     }
     else {
         return 1.0;
@@ -97,16 +121,20 @@ pub fn get_substitution_cost(a: char, b: char) -> f32 {
 }
 
 // NOTE: Make sure 2(transposition_cost) ≥ (insetion_cost)+(deletion_cost) for all letters
+// FIXME: this isn't respected!
 pub fn get_transposition_cost(a: char, b: char) -> f32 {
     if a == b {
         return 0.0;
+    }
+    else if are_characters_adjacent_on_keyboard(a, b) {
+        return ADJACENT_TRANSPOSITION_COST;
     }
     else {
         return 1.0;
     }
 }
 
-/// Returs the last index of c in s[0:max_i], -1 if not found
+/// Returs the last index of c in s\[0:max_i\], -1 if not found
 fn last_letter_id(s: &Vec<char>, c: char, max_i: usize) -> Option<usize> {
     for i in (0..max_i).rev() {
         if s[i] == c {
@@ -115,4 +143,50 @@ fn last_letter_id(s: &Vec<char>, c: char, max_i: usize) -> Option<usize> {
     }
 
     return None;
+}
+
+pub fn are_characters_equal_with_diacritics(a: char, b: char) -> bool {
+    let a_decoded = unidecode::unidecode(&String::from(a));
+    let b_decoded = unidecode::unidecode(&String::from(b));
+
+    return (a_decoded.len() == 1 && a_decoded.chars().next().unwrap() == b)
+        || (b_decoded.len() == 1 && b_decoded.chars().next().unwrap() == a);
+}
+
+/// FIXME: it's only for my keyboard
+/// Returns true if letter key shares a border on the keyboard
+pub fn are_characters_adjacent_on_keyboard(a: char, b: char) -> bool {
+    let lines = ["azertyuiop^", "qsdfghjklmù", "<wxcvbn,;:!"];
+    let is_middle_line_shifted_of_right = true;
+
+    // Get letter position
+    for i in 0..3 {
+        let char_vec: Vec<char> = lines[i].chars().collect();
+        let a_index = last_letter_id(&char_vec, a, char_vec.len());
+
+        match a_index {
+            Some(id) => {
+                if id < char_vec.len() - 1 && char_vec[id + 1] == b { return true; }
+                if id > 0 && char_vec[id - 1] == b { return true; }
+
+                if is_middle_line_shifted_of_right {
+                    let shift_up: i32 = [0, 0, -1][i];
+                    let shift_down: i32 = [-1, 0, 0][i];
+
+                    let int_id = id as i32;
+                    if i > 0 && int_id + shift_up >= 0 && lines[i - 1].chars().nth((int_id + shift_up) as usize) == Some(b) { return true; }
+                    if i > 0 && int_id + shift_up >= 0 && lines[i - 1].chars().nth((int_id + shift_up + 1) as usize) == Some(b) { return true; }
+                    if i < 2 && int_id + shift_down >= 0 && lines[i + 1].chars().nth((int_id + shift_down) as usize) == Some(b) { return true; }
+                    if i < 2 && int_id + shift_down >= 0 && lines[i + 1].chars().nth((int_id + shift_down + 1) as usize) == Some(b) { return true; }
+                }
+                else {
+                    if i > 0 && lines[i - 1].chars().nth(id) == Some(b) { return true; }
+                    if i < 2 && lines[i + 1].chars().nth(id) == Some(b) { return true; }
+                }
+            },
+            None => {},
+        }
+    } 
+
+    return false;
 }
--- src/main.rs
+++ src/main.rs
@@ -4,26 +4,81 @@ use std::fs;
 mod distance;
 mod vec2d;
 
+#[derive(Clone, Copy)]
+struct WordWithCost {
+    pub word_id: usize,
+    pub cost: f32,
+}
+
 fn main() {
     let args: Vec<String> = std::env::args().collect();
 
     let word = &args[1];
     let dict = read_words();
 
+    let start_time = std::time::Instant::now();
+    let mut skip_count = 0;
+
     // Compute distances
-    let mut bests = Vec::with_capacity(dict.len());
+    let best_count = 5;
+    let mut bests: Vec<WordWithCost> = Vec::with_capacity(best_count);
+    
     for i in 0..dict.len() {
+        // Get characters
+        let word_len = word.chars().count();
+        let other_len = dict[i].chars().count();
+
+        // Determine a minimum cost quickly with length
+        // It prevents calling get_distance unecessarily, and saves half of the time
+        let min_cost = if word_len > other_len {
+            (word_len - other_len) as f32 * distance::MIN_DELETION_COST
+        } else {
+            (other_len - word_len) as f32 * distance::MIN_INSERTION_COST
+        };
+
+        if bests.len() == best_count && min_cost >= bests[best_count - 1].cost {
+            skip_count += 1;
+            continue; // Not good enough!
+        }
+
         let dist = distance::get_distance(word, &dict[i]);
-        bests.push((dist, &dict[i]));
+
+        // Insert into bests
+        if bests.len() == 0 {
+            bests.push(WordWithCost {
+                word_id: i,
+                cost: dist,
+            });
+        }
+        else {
+            for j in (0..bests.len()).rev() {
+                if bests[j].cost >= dist {
+                    if j < bests.len() - 1 {
+                        bests[j + 1] = bests[j];
+                    }
+                    else if bests.len() < best_count {
+                        bests.push(bests[j]);
+                    }
+    
+                    bests[j] = WordWithCost {
+                        word_id: i,
+                        cost: dist,
+                    };
+                }
+                else { break; }
+            }
+        }
     }
 
-    // Sort by distance
-    bests.sort_by(|(a, _), (b, _)| f32::total_cmp(a, b));
+    println!("{}ms, {}ns/word, {} skipped", 
+        start_time.elapsed().as_millis(),
+        start_time.elapsed().as_nanos() / dict.len() as u128, 
+        skip_count
+    );
 
     // Show result
-    for i in 0..5 {
-        let (d, w) = bests[i];
-        println!("{}\t{}", w, d);
+    for i in 0..best_count {
+        println!("{}\t{}", dict[bests[i].word_id], bests[i].cost);
     }
 }
 
